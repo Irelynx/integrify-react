4c3c4d5d1b1175046fbf242f15c373f8be1c245c
Irelynx <slavakyi1997@gmail.com>
2023-02-24 17:03:47 +0200

    deploy configuration

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -3,24 +3,30 @@ import MainLayout from '~/layouts/main';
 import Countries from '~/pages/countries';
 import Country from '~/pages/country';
 import Error from '~/pages/error';
+import { basepath } from '~/settings';
 
-export const router = createBrowserRouter([
+export const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <MainLayout />,
+      errorElement: <Error />,
+      children: [
+        {
+          path: '',
+          element: <Countries />,
+        },
+        {
+          path: 'country/:id',
+          element: <Country />,
+        },
+      ],
+    },
+  ],
   {
-    path: '/',
-    element: <MainLayout />,
-    errorElement: <Error />,
-    children: [
-      {
-        path: '',
-        element: <Countries />,
-      },
-      {
-        path: 'country/:id',
-        element: <Country />,
-      },
-    ],
+    basename: basepath,
   },
-]);
+);
 
 export function Router() {
   return <RouterProvider router={router} />;
---
+++ src/settings.ts
@@ -0,0 +1,2 @@
+export const basepath =
+  import.meta.env.BASE_URL || window.location.href.includes('github.') ? '/integrify-react/' : '/';
