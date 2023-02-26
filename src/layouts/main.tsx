9d1dc4d714a296f4844572b7ad20d28e9e823f1e
Irelynx <slavakyi1997@gmail.com>
2023-02-26 23:58:23 +0200

    errorMessage alert added

--- src/layouts/main.tsx
+++ src/layouts/main.tsx
@@ -1,12 +1,25 @@
+import Alert from '@mui/material/Alert';
+import { useContext } from 'react';
 import { Outlet } from 'react-router-dom';
 
 import Header from '~/components/header';
+import { Context } from '~/contexts/app';
 
 export default function MainLayout() {
+  const { message } = useContext(Context);
   return (
     <>
       <Header />
       <main>
+        {message ? (
+          <div className='alerts-container'>
+            <Alert variant='filled' severity='warning'>
+              {message}
+            </Alert>
+          </div>
+        ) : (
+          ''
+        )}
         <Outlet />
       </main>
     </>
